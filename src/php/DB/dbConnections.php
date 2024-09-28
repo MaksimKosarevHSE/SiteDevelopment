8b4280775f6e303d3f05f7f1f131183d5480bb36
Max <maksimkosarev96@gmail.com>
2024-09-28 19:23:34 +0300

    Update dbConnection.php

---
+++ src/php/DB/dbConnections.php
@@ -0,0 +1,23 @@
+<?php
+const HOST = "server190.hosting.reg.ru";
+const USER_NAME = "u2837693_default";
+const PASSWORD = "IC8r6QJBJk5o2vaE";
+
+function getServerConnection() : PDO{
+    try{
+        $conn = new PDO("mysql:host=" . HOST, USER_NAME, PASSWORD);
+        return $conn;
+    } catch(PDOException $e){
+        die("Фатальная ошибка сервиса, обратитесь к администрации сайта");
+    }
+}
+
+function getDbConnection(string $dbName) : PDO{
+    try{
+        $conn = new PDO("mysql:host=" . HOST . ";dbname=$dbName", USER_NAME, PASSWORD);
+        return $conn;
+    } catch(PDOException $e){
+        die("Фатальная ошибка сервиса, обратитесь к администрации сайта");
+    }
+
+}
