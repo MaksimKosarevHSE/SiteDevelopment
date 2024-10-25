cb49db7f9373f4220fbbf6446561745da92b0158
Max <maksimkosarev96@gmail.com>
2024-10-25 22:49:47 +0300

    update

--- Personal_account/plan.php
+++ Personal_account/plan.php
@@ -7,7 +7,7 @@ $plan = json_decode(getPlan());
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../src/css/main.css">
     <link rel="stylesheet" href="../src/resources/fonts/Georgia/stylesheet.css" />
 	<link rel="stylesheet" href="../src/resources/fonts/Lato/stylesheet.css" />
@@ -34,79 +34,199 @@ $plan = json_decode(getPlan());
                 <h2 class="title">Расписание</h2>
 
             <p class="desc">Понедельник</p>
-                <p class="desc"></p>  
-                <p class="lyvovka">Львовка</p>       
-                <div class="containert mtb-3">
-                  <div class="table-responsive">
-                    <table class="table">
-                      <thead>
-                        <tr>
-                          <th>Время занятий</th>
-                          <th>Название предмета</th>
-                          <th>Преподаватель</th>
-                          <th>Кабинет</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>9:30-10:50</td>
-                          <td>Математический анализ</td>
-                          <td>Сараев</td>
-                          <td>216</td>
-                        </tr>
-                        <tr>
-                          <td>11:10-12:30</td>
-                          <td>Дискретная математика</td>
-                          <td>Логвинова Кира Владимировна</td>
-                          <td>303</td>
-                        </tr>
-                        <tr>
-                          <td>13:00-14:20</td>
-                          <td>Линейная алгебра и геометрия</td>
-                          <td>Шапошников</td>
-                          <td>206</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                  <p class="desc nextd">Вторник</p>  
-                <p class="desc"></p>  
-                <p class="rodionova">Родионова</p>       
-                <div class="containert mtb-3">
-                  <div class="table-responsive">
-                    <table class="table">
-                      <thead>
-                        <tr>
-                          <th>Время занятий</th>
-                          <th>Название предмета</th>
-                          <th>Преподаватель</th>
-                          <th>Кабинет</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>9:30-10:50</td>
-                          <td>Математический анализ</td>
-                          <td>Сараев</td>
-                          <td>216</td>
-                        </tr>
-                        <tr>
-                          <td>11:10-12:30</td>
-                          <td>Дискретная математика</td>
-                          <td>Логвинова Кира Владимировна</td>
-                          <td>303</td>
-                        </tr>
-                        <tr>
-                          <td>13:00-14:20</td>
-                          <td>Линейная алгебра и геометрия</td>
-                          <td>Шапошников</td>
-                          <td>206</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-        </div>
+                    <p class="desc"></p>
+                    <p class="lyvovka">Ура, начало учёбы!</p>
+                    <div class="containert mtb-3">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>Время занятий</th>
+                                    <th>Название предмета</th>
+                                </tr>
+                                </thead>
+                                <tbody id="mon">
+                                <tr>
+                                    <td>9:30-10:50</td>
+                                    <td>Математический анализ</td>
+                                </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+
+
+
+
+                        <p class="desc">Вторник</p>
+                        <p class="desc"></p>
+                        <p class="lyvovka">Не унывай!</p>
+                        <div class="containert mtb-3">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th>Время занятий</th>
+                                        <th>Название предмета</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody id="tue">
+                                    <tr>
+                                        <td>9:30-10:50</td>
+                                        <td>Математический анализ</td>
+                                    </tr>
+                                    <tr>
+                                        <td>11:10-12:30</td>
+                                        <td>Дискретная математика</td>
+                                    </tr>
+                                    <tr>
+                                        <td>13:00-14:20</td>
+                                        <td>Линейная алгебра и геометрия</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+
+
+
+                            <p class="desc">Среда</p>
+                            <p class="desc"></p>
+                            <p class="lyvovka">Ботаем, ботаем и еще раз ботаем!</p>
+                            <div class="containert mtb-3">
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                        <tr>
+                                            <th>Время занятий</th>
+                                            <th>Название предмета</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody id="third">
+                                        <tr>
+                                            <td>9:30-10:50</td>
+                                            <td>Математический анализ</td>
+                                        </tr>
+                                        <tr>
+                                            <td>11:10-12:30</td>
+                                            <td>Дискретная математика</td>
+                                        </tr>
+                                        <tr>
+                                            <td>13:00-14:20</td>
+                                            <td>Линейная алгебра и геометрия</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+
+
+
+
+                                <p class="desc">Четверг</p>
+                                <p class="desc"></p>
+                                <p class="lyvovka">Пятница близко!</p>
+                                <div class="containert mtb-3">
+                                    <div class="table-responsive">
+                                        <table class="table">
+                                            <thead>
+                                            <tr>
+                                                <th>Время занятий</th>
+                                                <th>Название предмета</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody id="thur">
+                                            <tr>
+                                                <td>9:30-10:50</td>
+                                                <td>Математический анализ</td>
+                                            </tr>
+                                            <tr>
+                                                <td>11:10-12:30</td>
+                                                <td>Дискретная математика</td>
+                                            </tr>
+                                            <tr>
+                                                <td>13:00-14:20</td>
+                                                <td>Линейная алгебра и геометрия</td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+
+
+                                    <p class="desc">Пятница</p>
+                                    <p class="desc"></p>
+                                    <p class="lyvovka">Ура, пятница!</p>
+                                    <div class="containert mtb-3">
+                                        <div class="table-responsive">
+                                            <table class="table">
+                                                <thead>
+                                                <tr>
+                                                    <th>Время занятий</th>
+                                                    <th>Название предмета</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody id="fri">
+                                                <tr>
+                                                    <td>9:30-10:50</td>
+                                                    <td>Математический анализ</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>11:10-12:30</td>
+                                                    <td>Дискретная математика</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>13:00-14:20</td>
+                                                    <td>Линейная алгебра и геометрия</td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+
+
+                                        <p class="desc">Суббота</p>
+                                        <p class="desc"></p>
+                                        <p class="lyvovka">Мы дожили!</p>
+                                        <div class="containert mtb-3">
+                                            <div class="table-responsive">
+                                                <table class="table">
+                                                    <thead>
+                                                    <tr>
+                                                        <th>Время занятий</th>
+                                                        <th>Название предмета</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody id="sut">
+                                                    <tr>
+                                                        <td>9:30-10:50</td>
+                                                        <td>Математический анализ</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>11:10-12:30</td>
+                                                        <td>Дискретная математика</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>13:00-14:20</td>
+                                                        <td>Линейная алгебра и геометрия</td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+
+
+                                        </div>
+                                        <div id="d123"></div>
     </section>
+<script>
+    $(function(){
+        group = "knt9";
+        if (planFromServer.length != 0){
+            localPlan = planFromServer["mon"]["knt9"];
+            console.log(localPlan);
+        }
+        $("#d123").append();
+    });
+
+</script>
 </body>
 </html>
 
